4be89b15cd18f8711dfc9df6f3c3945d87518f8e
Dani1aw3 <uhrindani100@gmail.com>
2026-05-05 16:43:44 +0200

    sok

--- Assets/Scripts/DudePersonScript.gd
+++ Assets/Scripts/DudePersonScript.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var sprite: Sprite2D = $Marker2D/Sprite2D
+@onready var position2D=$Marker2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +13,19 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("fel") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("bal", "jobb")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	if direction>0:
+		position2D.scale.x=1
+	elif direction<0:
+		position2D.scale.x=-1
 	move_and_slide()
